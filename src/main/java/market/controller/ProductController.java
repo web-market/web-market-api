2fd02dfac592e2d0c262aad140e83955bafce806
AndrewZabur <andrewzabur@gmail.com>
2020-06-13 00:12:39 +0300

    Merged successfully.

--- src/main/java/market/controller/ProductController.java
+++ src/main/java/market/controller/ProductController.java
@@ -1,24 +1,65 @@
 package market.controller;
 
 import lombok.RequiredArgsConstructor;
+import market.dto.BulkDeleteDto;
+import market.dto.product.ProductDto;
+import market.dto.transfer.Create;
+import market.dto.transfer.Update;
 import market.entity.Product;
 import market.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/product")
+@RequestMapping(path = "/products")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/all")
+    @GetMapping
     public List<Product> getAll() {
         return this.productService.getAll();
     }
 
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return this.productService.findOneById(id);
+    }
+
+    @GetMapping("/by-categories/{ids}")
+    public List<Product> getByCategoryIds(@PathVariable Long[] ids) {
+        return this.productService.getAllByCategories(Arrays.asList(ids));
+    }
+
+    @GetMapping("/by-filter-values/{ids}")
+    public List<Product> getByFilerValueIds(@PathVariable Long[] ids) {
+        return this.productService.getAllByFilterValues(Arrays.asList(ids));
+    }
+
+    @GetMapping("/by-name/{name}")
+    public List<Product> getByNameLike(@PathVariable String name) {
+        return this.productService.getAllByNameLike(name);
+    }
+
+    @PostMapping
+    public Product create(@Validated(Create.class) @RequestBody ProductDto productDto) {
+        return this.productService.create(productDto);
+    }
+
+    @PutMapping
+    public Product update(@Validated(Update.class) @RequestBody ProductDto productDto) {
+        return this.productService.update(productDto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete(@Valid @RequestBody BulkDeleteDto bulkDeleteDto) {
+        this.productService.bulkDelete(bulkDeleteDto.getIds());
+        return ResponseEntity.ok("deleted successfully");
+    }
 }
--- src/main/java/market/repository/ProductRepository.java
+++ src/main/java/market/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package market.repository;
 import market.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Product findOneById(Long id);
